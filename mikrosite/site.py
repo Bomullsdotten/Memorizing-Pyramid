926c7ba229fb89dc37b4f24c80122ccb94700a1f
Sondre Erstad <sondre_erstad@hotmail.com>
2017-02-16 12:24:01 +0100

    First try?

---
+++ mikrosite/site.py
@@ -0,0 +1,18 @@
+from wsgiref.simple_server import make_server
+
+from pyramid.response import Response
+from pyramid.config import Configurator
+
+def hi(request):
+    print("geting the hands of it now.")
+    return Response("<body><h1>Shld i change things up?</h1></body>")
+
+
+if __name__ == '__main__':
+    config = Configurator()
+    config.add_route('hi', '/')
+    config.add_view(hi, route_name='hi')
+
+    app = config.make_wsgi_app()
+    server = make_server('0.0.0.0', 6500, app=app)
+    server.serve_forever()
