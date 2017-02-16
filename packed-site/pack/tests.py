a8a0331c7e07b0e5c19d7382497cb3583e6e3e04
Sondre Erstad <sondre_erstad@hotmail.com>
2017-02-16 12:16:37 +0100

    functional testing

--- packed-site/pack/tests.py
+++ packed-site/pack/tests.py
@@ -10,13 +10,25 @@ class MyTestCase(unittest.TestCase):
     def tearDown(self):
         testing.tearDown()
 
-    def test_something(self):
+    def test_hello_world(self):
         from pack import hello_world
         request = testing.DummyRequest()
         response = hello_world(request)
         self.assertEqual(response.status_code, 200)
 
 
+class FunctionalTestCase(unittest.TestCase):
+    def setUp(self):
+        from pack import main
+        app = main({})
+        from webtest import TestApp
+
+        self.testapp = TestApp(app)
+
+    def test_hello_world(self):
+
+        response = self.testapp.get('/', status=200)
+        self.assertIn(b'<h1>testing</h1>', response.body)
 
 
 if __name__ == '__main__':
