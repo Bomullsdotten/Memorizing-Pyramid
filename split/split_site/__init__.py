dbdb970723f96aaff90894d951bd2d58167bced3
Sondre Erstad <sondre_erstad@hotmail.com>
2017-02-27 12:59:33 +0100

    lockup

--- split/split_site/__init__.py
+++ split/split_site/__init__.py
@@ -3,6 +3,7 @@ from pyramid.config import Configurator
 
 def main(global_values, **settings):
     config = Configurator(settings=settings)
+    config.include('pyramid_jinja2')
     config.add_route('home', '/')
     config.add_route('hello', '/hello')
     config.scan('.views')
