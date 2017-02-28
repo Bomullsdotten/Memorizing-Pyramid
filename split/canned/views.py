e4725721997684521f4238e962e0f857c5a1167c
Sondre Erstad <sondre_erstad@hotmail.com>
2017-02-28 14:34:03 +0100

    Added loggs

--- split/canned/views.py
+++ split/canned/views.py
@@ -1,8 +1,12 @@
+import logging
+log = logging.getLogger(__name__)
+
 from pyramid.view import view_config
 
 
 @view_config(route_name='home', renderer='base.jinja2')
 def home(request):
+    log.debug('In home view')
     return dict(
         title='Home',
         text='I am home'
@@ -11,6 +15,7 @@ def home(request):
 
 @view_config(route_name='screen', renderer='base.jinja2')
 def screen(request):
+    log.debug('In screen view')
     return dict(
         title='Screen',
         text='Hello my dear screen'
